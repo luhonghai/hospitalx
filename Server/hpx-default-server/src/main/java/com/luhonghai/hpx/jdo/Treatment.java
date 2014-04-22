c162009d300ce523a5f599619df81b4d2d93cb19
hailucmg <hai.lu@c-mg.com>
2014-04-22 16:25:54 +0700

    Updatge

---
+++ Server/hpx-default-server/src/main/java/com/luhonghai/hpx/jdo/Treatment.java
@@ -0,0 +1,59 @@
+package com.luhonghai.hpx.jdo;
+
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+import java.util.Date;
+
+/**
+ * Created by Hai Lu on 22/04/2014.
+ */
+
+@PersistenceCapable
+public class Treatment {
+    @PrimaryKey
+    @Persistent(customValueStrategy="uuid")
+    private String id;
+
+    @Persistent
+    private String type;
+
+    @Persistent
+    private Date createdDate;
+
+    @Persistent
+    private Patient patient;
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+}
